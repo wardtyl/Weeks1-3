75398dc9b160948107a997747bfcc26f7914874b
SHERNET\wardtyl <wardtyl@sheridancollege.ca>
2026-01-22 15:52:14 -0500

    Beginning of Mechanical Drawing, trying to experiment with mapping of moon following the mouse

---
+++ Assets/Scripts/Moon Follows Mouse.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoonFollowsMouse : MonoBehaviour
+{
+    public Camera gameCamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentMousePosition = Mouse.current.position.ReadValue();
+        Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
+        worldMousePosition.z = 0;
+        transform.position = worldMousePosition;
+    }
+}
