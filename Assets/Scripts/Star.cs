386ba2ed9dee80b567abd396d5d0b91db365ab2c
wardtyl <wardtyl@sheridancollege.ca>
2026-01-30 10:51:54 -0500

    Comments made for star script

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Star : MonoBehaviour
 {
-    public float duration;
     public float size;
+    public float period;
     public float progress = 0f;
     public AnimationCurve curve;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,9 +16,11 @@ public class Star : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        progress += Time.deltaTime / duration;
+        //Progress indicates the progress along the animation curve (similar to a percentage value/bar).
+        //Period represents how long I want the animation curve to take over a period of time.
+        //I have used the values within my animation curve to determine the size of the object. The size/scale is determined by progress resetting between 1f and 0f.
+        progress += Time.deltaTime / period;
         size = curve.Evaluate(progress);
-
         transform.localScale = Vector3.one * size;
 
         if (progress > 1f)
