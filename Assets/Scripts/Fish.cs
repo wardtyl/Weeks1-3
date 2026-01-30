495f9578e7df2c371eb4a48a6d523e9942639cf4
wardtyl <wardtyl@sheridancollege.ca>
2026-01-30 11:19:45 -0500

    Comments added to fish and moon. Also changed the value of XMax and XMin in fish and made said values private

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -2,29 +2,30 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+    private float xMax;
+    private float xMin;
     public float speed;
-    public float xMax;
-    public float xMin;
     public Camera gameCamera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Setting Limitations for fish so it will not bounce off screen
-        xMax = Screen.width;
-        xMin = 0;
+        //Setting Limitations for fish so it will bounce between two designated points within the screen
+        xMax = 900f;
+        xMin = 200f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Need to create a Vector3 which will embody the values of transform.position
         Vector3 moverXPos = transform.position;
         moverXPos.x += speed * Time.deltaTime;
         transform.position = moverXPos;
 
         Vector3 screenTransformPosition = gameCamera.WorldToScreenPoint(transform.position);
 
-        //if fish is about to go over either side of the screen, reverse the direction
+        //if fish is about to go over either of the designated values for XMax or XMin, reverse the direction
         if (xMax < screenTransformPosition.x)
         {
             speed *= -1;
--- Assets/Scripts/Moon.cs
+++ Assets/Scripts/Moon.cs
@@ -14,8 +14,11 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Created Vector 3 ensures that the object coordinates its own position with the mouse position. 
         Vector3 currentMousePosition = Mouse.current.position.ReadValue();
         Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
+        //Ensures object is visible on game screen and stays on the designated y-value while still following the mouse.
+        worldMousePosition.y = 3.4f;
         worldMousePosition.z = 1;
         transform.position = worldMousePosition;
     }
